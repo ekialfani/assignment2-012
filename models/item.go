b8c76c388e8960d16d07fa9c220966b4fddea2d8
ekialfani <ekialfani15@gmail.com>
2023-09-20 11:44:52 +0800

    feat: Add Order and Item models

---
+++ models/item.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type Item struct {
+	ID uint `gorm:"primaryKey"`
+	ItemCode string `gorm:"not null; unique;type:varchar(191)" json:"item_code"`
+	Description string `json:"description"`
+	Quantity uint `gorm:"not null; default:0" json:"quantity"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
