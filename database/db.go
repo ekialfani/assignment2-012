ce82d5550bc6ad802bf38f36e805a838e777c3c2
ekialfani <ekialfani15@gmail.com>
2023-09-23 23:37:15 +0800

    ref: database configuration and add environment variables

--- database/db.go
+++ database/db.go
@@ -1,25 +1,20 @@
 package database
 
 import (
+	"assignment2-012/config"
 	"assignment2-012/models"
-	"fmt"
 	"log"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var (
-	host = "localhost"
-	user = "postgres"
-	password = "postgres"
-	port = 5432
-	dbname = "myapp_db"
 	db *gorm.DB
 	err error
 )
 
 func StartDB() {
-	config := fmt.Sprintf("host=%s user=%s password=%s port=%d dbname=%s sslmode=disable", host, user, password, port, dbname)
+	config := config.GetDBConfig()
 
 	db, err = gorm.Open(postgres.Open(config), &gorm.Config{})
 
